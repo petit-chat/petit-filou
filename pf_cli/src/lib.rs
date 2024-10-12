21981f30c9f54dcd20c2583d58894ebb5f565072
petit-chat <76547570+petit-chat@users.noreply.github.com>
2024-10-12 12:22:09 +0200

    feat: automated release management with ci/cd workflow (#5)

--- pf_cli/src/lib.rs
+++ pf_cli/src/lib.rs
@@ -4,26 +4,26 @@ use clap::{Parser, ValueEnum};
 #[derive(Parser)]
 #[command(name = "pf", author, version, about)]
 pub struct Opt {
-    /// WordPress base URL (e.g. http://example.com).
+    /// WordPress base URL (e.g. <http://example.com>).
     pub url: String,
 
     /// Searching mode.
     #[arg(value_enum)]
     pub mode: Mode,
 
-    /// Result set published before a given date (cf. https://core.trac.wordpress.org/ticket/41032).
+    /// Result set published before a given date (cf. <https://core.trac.wordpress.org/ticket/41032>).
     #[arg(long)]
     pub before: Option<String>,
 
-    /// Result set modified before a given date (cf. https://core.trac.wordpress.org/ticket/41032).
+    /// Result set modified before a given date (cf. <https://core.trac.wordpress.org/ticket/41032>).
     #[arg(long)]
     pub modified_before: Option<String>,
 
-    /// Result set published after a given date (cf. https://core.trac.wordpress.org/ticket/41032).
+    /// Result set published after a given date (cf. <https://core.trac.wordpress.org/ticket/41032>).
     #[arg(long)]
     pub after: Option<String>,
 
-    /// Result set modified after a given date (cf. https://core.trac.wordpress.org/ticket/41032).
+    /// Result set modified after a given date (cf. <https://core.trac.wordpress.org/ticket/41032>).
     #[arg(long)]
     pub modified_after: Option<String>,
 
